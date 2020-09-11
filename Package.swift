158b0c8e0ce0d99926a0d2fd05cf8900dedeb2de
Ricardo Pereira <m@ricardopereira.eu>
2020-09-11 09:44:51 +0100

    Tentative fix of: reference to member 'v9' cannot be resoved without a contextual type

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v10_13),
         .iOS(.v11),
         .tvOS(.v11),
-        .watchOS(.v9)
+        .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
